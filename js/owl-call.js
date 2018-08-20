be8ca356fd8c637c5b7781b3eeaa7e62395a74b3
Uncle Vader <unclevader@gmail.com>
2018-08-20 22:03:56 +0300

    page refactoring & bug fixes

--- js/owl-call.js
+++ js/owl-call.js
@@ -1,24 +1,27 @@
-$(document).ready(function(){
-    $('.owl-carousel').owlCarousel({
-        items: 1,
+$(document).ready(function () {
+  $('.owl-carousel').owlCarousel({
+    items: 1,
+    loop: true,
+    autoWidth: false,
+    center: true,
+    stagePadding: 0,
+    dots: true,
+    margin: 25,
+    touchDrag: true,
+    mouseDrag: true,
+    responsive: {
+      600: {
+        items: 3,
         loop: false,
-        autoWidth: false,
-        center: true,
-        stagePadding: 0,
-        dotsData: true,
-        margin: -80,
-        responsive:{
-            600:{
-                items:3,
-                loop:false,
-                touchDrag: false,
-                center: false,
-                mouseDrag: false
-            }
-        }
-    });
-    $('.owl-carousel').on('changed.owl.carousel', function(e) {
-      if (!e.namespace || e.property.name != 'position') return
-      $('#info').text(e.relatedTarget.relative(e.item.index) + '/' + e.item.count)
-    })
+        touchDrag: false,
+        mouseDrag: false,
+        center: false,
+      },
+
+    }
+  });
+  $('.owl-carousel').on('changed.owl.carousel', function (e) {
+    if (!e.namespace || e.property.name != 'position') return
+    $('#info').text(e.relatedTarget.relative(e.item.index) + '/' + e.item.count)
+  })
 });

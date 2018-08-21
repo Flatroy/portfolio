e01016951016bb6eb1288545462b2b6a744a2334
Uncle Vader <unclevader@gmail.com>
2018-08-21 11:12:33 +0300

    tooltip issues fixed

--- js/tooltip-call.js
+++ js/tooltip-call.js
@@ -1,3 +1,20 @@
-$(document).ready(function(){
-  $('.tooltip').tooltipster();
+$(document).ready(function () {
+  $('.tooltip').tooltipster({
+    windowOffset: {
+      bottom: 10,
+      left: 10,
+      right: 10,
+      top: 10
+    },
+    trigger: 'custom',
+    triggerOpen: {
+      mouseenter: true,
+      touchstart: true
+    },
+    triggerClose: {
+      click: true,
+      scroll: true,
+      tap: true
+    }
+  });
 });
